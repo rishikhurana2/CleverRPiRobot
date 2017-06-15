3708b558a93d8c752ca85e9e6b99d4c9fc422d37
ianleeeee <league@WTS19.attlocal.net>
2017-06-15 12:02:33 -0700

    added javaDoc

--- src/org/jointheleague/ecolban/cleverrobot/Camera.java
+++ src/org/jointheleague/ecolban/cleverrobot/Camera.java
@@ -12,6 +12,14 @@ public class Camera extends RPiCamera {
 	int width;
 	int height;
 
+	/**
+	 * 
+	 * @param width
+	 * @param height
+	 * @throws FailedToRunRaspistillException
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	Camera(int width, int height) throws FailedToRunRaspistillException, IOException, InterruptedException {
 		super("/home/pi/Desktop");
 		this.width = width;
@@ -19,6 +27,10 @@ public class Camera extends RPiCamera {
 
 	}
 
+	/**
+	 * this method takes a picture, using the RPi camera, as an array of RGB
+	 * values
+	 */
 	public void takeRGBPicture() {
 		try {
 			pixels = this.takeStillAsRGB(width, height, false);
@@ -27,10 +39,26 @@ public class Camera extends RPiCamera {
 		}
 	}
 
+	/**
+	 * this method returns the array of RGB values taken by the RPi camera
+	 * 
+	 * @return returns the array of RGB values
+	 */
 	public int[] getPixels() {
 		return pixels;
 	}
 
+	/**
+	 * this method gets amount of red in the taken image (in percentage form)
+	 * 
+	 * @param redThreshold,
+	 *            the value representing what is considered red
+	 * @param debug,
+	 *            whether or not the image prints out 1s and 0s based on if the
+	 *            pixels are red or not (used for debug purposes)
+	 * @return returns the number of red pixels divided by the total amount
+	 *         pixels (as a percentage)
+	 */
 	public double getRedPercentage(int redThreshold, boolean debug) {
 		// returns the percentage of red pixels / total pixels
 
@@ -67,7 +95,7 @@ public class Camera extends RPiCamera {
 		if (debug) {
 			int count = 0;
 			for (boolean pix : redPixels) {
-				
+
 				if (pix) {
 					System.out.print(1);
 				} else {
@@ -79,11 +107,23 @@ public class Camera extends RPiCamera {
 				count++;
 			}
 		}
+		System.out.println();
 
 		double redPercentage = (double) trueRedPixels / numPixels;
 		return redPercentage * 100;
 	}
 
+	/**
+	 * this method gets amount of blue in the taken image (in percentage form)
+	 * 
+	 * @param blueThreshold,
+	 *            the value representing what is considered blue
+	 * @param debug,
+	 *            whether or not the image prints out 1s and 0s based on if the
+	 *            pixels are blue or not (used for debug purposes)
+	 * @return returns the number of blue pixels divided by the total amount
+	 *         pixels (as a percentage)
+	 */
 	public double getBluePercentage(int blueThreshold, boolean debug) {
 		// returns the percentage of blue pixels / total pixels
 		int numPixels = (pixels.length) / 3;
@@ -131,13 +171,24 @@ public class Camera extends RPiCamera {
 				count++;
 			}
 		}
-
+		System.out.println();
 		double bluePercentage = (double) trueBluePixels / numPixels;
 		return bluePercentage * 100;
 	}
 
+	/**
+	 * this method gets amount of green in the taken image (in percentage form)
+	 * 
+	 * @param greenThreshold,
+	 *            the value representing what is considered green
+	 * @param debug,
+	 *            whether or not the image prints out 1s and 0s based on if the
+	 *            pixels are green or not (used for debug purposes)
+	 * @return returns the number of green pixels divided by the total amount
+	 *         pixels (as a percentage)
+	 */
 	public double getGreenPercentage(int greenThreshold, boolean debug) {
-		// returns the percentage of green pixels / total pixels
+
 		int numPixels = (pixels.length) / 3;
 		boolean[] greenPixels = new boolean[numPixels];
 		for (int i = 0; i < pixels.length; i += 3) {
@@ -185,6 +236,7 @@ public class Camera extends RPiCamera {
 				count++;
 			}
 		}
+		System.out.println();
 		double greenPercentage = (double) trueGreenPixels / numPixels;
 		return greenPercentage * 100;
 	}
